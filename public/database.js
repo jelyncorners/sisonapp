c656a2f1aee8261630c644d6eed58d54d89f7087
Jelyn Corners <99438112+jelyncorners@users.noreply.github.com>
2022-12-07 21:02:41 -0800

    Update database.js

--- public/database.js
+++ public/database.js
@@ -1,12 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAUwAvZbQFc96i9jN5cJvvsQYeofNe5CDI",
-    authDomain: "jcornersbadbank-75a0b.firebaseapp.com",
-    databaseURL: "https://jcornersbadbank-75a0b-default-rtdb.firebaseio.com",
-    projectId: "jcornersbadbank-75a0b",
-    storageBucket: "jcornersbadbank-75a0b.appspot.com",
-    messagingSenderId: "777397107516",
-    appId: "1:777397107516:web:45127086b01a3ef9827c39",
-    measurementId: "G-0XTB37SEWV"
+    apiKey: "AIzaSyAPAZZy_saBmo4kioORW7lNnIw3S49b9aY",
+  authDomain: "sisonfirebase.firebaseapp.com",
+  projectId: "sisonfirebase",
+  storageBucket: "sisonfirebase.appspot.com",
+  messagingSenderId: "747239577497",
+  appId: "1:747239577497:web:c387388939a73f5627a508"
 };
 
 try {
